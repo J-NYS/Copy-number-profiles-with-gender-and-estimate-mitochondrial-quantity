a6d5eed5ba0e884ca9030a38d74b4e82eb7659bf
J-NYS <jens_nys@msn.com>
2016-06-08 15:50:53 +0200

    Update and rename pipeline-QDNAseq_MQA_CMD_GenderDeterm.sh to pipeline-QDNAseq_MQA_CMD.sh

--- pipeline-QDNAseq_MQA_CMD.sh
+++ pipeline-QDNAseq_MQA_CMD.sh
@@ -2,26 +2,27 @@
 #PBS -l nodes=1:ppn=4
 
 # Read Sample name & Make Dir
-#	mkdir "$1"
-#	echo ""
+	mkdir "$1"
+	mkdir GenderDetermination
+	echo ""
 
 # Using QDNAseq R script
-#	echo "Running Rscript_QDNAseq"
-#	Rscript QDNAseq.R $1 $2
-#	echo ""
+	echo "Running Rscript_QDNAseq"
+	Rscript QDNAseq.R $1 $2
+	echo ""
 
 # Mitochondrial Quantity analysis
 #	echo "Mitochondrial Quantity analysis"
 	cd $2
 	samtools view -cq 30 $1.bam > $1reads.txt
-#	samtools view -cq 30 $1.bam MT > $1readsMT.txt
-#	cd -; echo $?
-#	Rscript MitochondrialQuantityAnalyse.R $1 $2
-#	cd $2
-#	rm $1reads*
-#	cd -; echo $?
-#	mv *MitochondrialQuantityAnalyse.txt $1
-#	echo ""
+	samtools view -cq 30 $1.bam MT > $1readsMT.txt
+	cd -; echo $?
+	Rscript MitochondrialQuantityAnalyse.R $1 $2
+	cd $2
+	rm $1reads*
+	cd -; echo $?
+	mv *MitochondrialQuantityAnalyse.txt $1
+	echo ""
 
 # Gender Determitation
 	echo "Gender Determitation"
