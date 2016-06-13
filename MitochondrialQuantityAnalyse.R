fedbf73fc6604efba1810316d703ed506fec64f4
J-NYS <jens_nys@msn.com>
2016-06-13 10:54:31 +0200

    Delete MitochondrialQuantityAnalyse.R

--- MitochondrialQuantityAnalyse.R
+++
@@ -1,9 +0,0 @@
-args <- commandArgs(TRUE)
-
-reads <- read.csv(file=paste(args[2],"/",args[1],"reads.txt",sep=""), header = FALSE)
-readsMT <- read.csv(file=paste(args[2],"/",args[1],"readsMT.txt",sep=""), header = FALSE)
-
-MQA <- readsMT$V1/reads$V1*1000000
-write.table(MQA, file = paste(args[1],"MitochondrialQuantityAnalyse.txt", sep="_"))
-
-print("END R-SCRIPT_MitochondrialQuantityAnalyse JNys")
